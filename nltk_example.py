e8a221e34e4bc0d841554c2b1f380062ebc1ba3c
Javeed Basha <jbharoon89@gmail.com>
2017-06-28 19:20:36 +0530

    Update nltk_example.py

--- nltk_example.py
+++ nltk_example.py
@@ -5,7 +5,10 @@
 
 # **What is NLTK?**
 # 
-# NLTK is a leading platform for building Python programs to work with human language data. It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, along with a suite of text processing libraries for classification, tokenization, stemming, tagging, parsing, and semantic reasoning.
+# NLTK is a leading platform for building Python programs to work with human language data. 
+# It provides easy-to-use interfaces to over 50 corpora and lexical resources such as WordNet, 
+# along with a suite of text processing libraries for classification, tokenization, stemming, 
+# tagging, parsing, and semantic reasoning.
 
 # ** Python Version: ** Python 2.7
 # 
